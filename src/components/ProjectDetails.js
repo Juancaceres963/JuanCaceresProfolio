39042afaba1d2674c5ce34c75f646dc9e217189f
Juan Cáceres <dnijuancaceres@gmail.com>
2025-07-16 04:45:36 -0300

    Ajustes de estructura ArojectDetails.js

--- src/components/ProjectDetails.js
+++ src/components/ProjectDetails.js
@@ -1,8 +1,20 @@
 import { useParams } from "react-router-dom";
 import { projects } from "./projectsData"; // ✅ Importamos la lista de proyectos
+import { Footer } from "./Footer.js";
+import { useEffect } from "react";
+import {
+  Container,
+  Carousel,
+  TabContainer,
+  TabContent,
+  Col,
+  Nav,
+  Tab,
+} from "react-bootstrap";
 import UnderConstructionModal from "../components/UnderConstructionModal";
 import "../styles/projectDetails.scss";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export const ProjectDetails = () => {
   const { id } = useParams(); // Obtener el ID desde la URL
@@ -13,104 +25,53 @@ export const ProjectDetails = () => {
   }
 
   return (
-    <div className="project-details">
-      <div className="project-details-text">
-        <UnderConstructionModal />
-        <h1>{project.title}</h1>
-        <p>{project.description}</p>
-        <h3>Tecnologías utilizadas:</h3>
-        <ul>
-          {project.technologies.map((tech, index) => (
-            <li key={index}>{tech}</li>
-          ))}
-        </ul>
-        <h3>Habilidades Aplicadas:</h3>
-        <p>{project.skills}</p>
-      </div>
-
-      <div id="carouselExampleCaptions" data-bs-interval="false" data-bs-ride="carousel" className="carousel slide">
-        <div className="carousel-indicators">
-          <button
-            type="button"
-            data-bs-target="#carouselExampleCaptions"
-            data-bs-slide-to="0"
-            className="active"
-            aria-current="true"
-            aria-label="Slide 1"
-          ></button>
-          <button
-            type="button"
-            data-bs-target="#carouselExampleCaptions"
-            data-bs-slide-to="1"
-            aria-label="Slide 2"
-          ></button>
-          <button
-            type="button"
-            data-bs-target="#carouselExampleCaptions"
-            data-bs-slide-to="2"
-            aria-label="Slide 3"
-          ></button>
+    <div>
+      <Container>
+        <div className="project-details-inicio">
+          {/* <UnderConstructionModal /> */}
+          <h1>{project.title}</h1>
+          <img src={project.mainImage} />
+          <p>{project.subtitle}</p>
         </div>
-        <div className="carousel-inner">
-          <div className="carousel-item active">
-            <img
-              className="projects-details-img d-block w-100"
-              src={project.imgUrl4}
-              alt={project.title}
-            />
-            <div className="carousel-caption d-none d-md-block">
-              <h5>First slide label</h5>
-              <p>
-                Some representative placeholder content for the first slide.
-              </p>
-            </div>
+        <div className="project-details-descripcion">
+          <ul>
+            {project.tags.map((tags, index) => (
+              <li key={index}>{tags}</li>
+            ))}
+          </ul>
+          <div className="temas">
+            <h3>Problem</h3>
+            <p>{project.problem}</p>
           </div>
-          <div className="carousel-item">
-            <img
-              className="projects-details-img d-block w-100"
-              src={project.imgUrl2}
-              alt={project.title}
-            />
-            <div className="carousel-caption d-none d-md-block">
-              <h5>Second slide label</h5>
-              <p>
-                Some representative placeholder content for the second slide.
-              </p>
-            </div>
+          <div className="temas">
+            <h3>Role</h3>
+            <p>{project.role}</p>
           </div>
-          <div className="carousel-item">
-            <img
-              className="projects-details-img d-block w-100"
-              src={project.imgUrl3}
-              alt={project.title}
-            />
-            <div className="carousel-caption d-none d-md-block">
-              <h5>Third slide label</h5>
-              <p>
-                Some representative placeholder content for the third slide.
-              </p>
-            </div>
+          <div className="temas">
+          <ul>
+            {project.tools.map((tech, index) => (
+              <li key={index}>{tech}</li>
+            ))}
+            </ul>
+          </div>
+          <Carousel interval={5000} fade>
+            {project.images.map((img, index) => (
+              <Carousel.Item key={index}>
+                <img
+                  src={img}
+                  className="d-block w-100"
+                  alt={`Slide ${index + 1}`}
+                />
+              </Carousel.Item>
+            ))}
+          </Carousel>
+          <div className="tools">
+            <p>{project.duration}</p>
+            <p>{project.type}</p>
           </div>
         </div>
-        <button
-          className="carousel-control-prev"
-          type="button"
-          data-bs-target="#carouselExampleCaptions"
-          data-bs-slide="prev"
-        >
-          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span className="visually-hidden">Previous</span>
-        </button>
-        <button
-          className="carousel-control-next"
-          type="button"
-          data-bs-target="#carouselExampleCaptions"
-          data-bs-slide="next"
-        >
-          <span className="carousel-control-next-icon" aria-hidden="true"></span>
-          <span className="visually-hidden">Next</span>
-        </button>
-      </div>
+      </Container>
+      <Footer />
     </div>
   );
 };
