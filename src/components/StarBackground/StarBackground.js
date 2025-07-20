4b3c8638621b880d7b24e17fd94a865cf1cc2b65
Juan Cáceres <dnijuancaceres@gmail.com>
2025-07-20 03:50:20 -0300

    Creando StarBackground component

---
+++ src/components/StarBackground/StarBackground.js
@@ -0,0 +1,14 @@
+// import './StarBackground.css';
+
+// export const StarBackground = () => {
+//   return <div className="stars"></div>;
+// }
+import './StarBackground.css';
+
+export const StarBackground = () => {
+  return (
+    <div className="stars">
+      <div className="stars-layer3"></div>
+    </div>
+  );
+}
