a6e27b4a3abc78813dd120c3f9087c9e61ccc7b3
Juan Cáceres <dnijuancaceres@gmail.com>
2025-08-01 00:04:51 -0300

    Ajustando orden de los certificados

--- src/components/Data/CertificatesData.js
+++ src/components/Data/CertificatesData.js
@@ -10,27 +10,27 @@ import certiImg7 from "../../assets/img/Certificates/Azure-Fundamentals.jpg";
 export const certificates = [
     {
     id: 1,
-    title: "Javascript fundamentals",
-    duration: "19h",
-    date: "October 2022",
-    link: "https://platzi.com/p/dnijuancaceres/curso/1814-course/diploma/detalle/",
-    image: certiImg1
+    title: "Azure Fundamentals",
+    duration: "60h",
+    date: "August 2023",
+    link: "https://www.credly.com/badges/ad1bb2df-d4d8-4399-9157-e15371cdef06/linked_in_profile",
+    image: certiImg7
     },
     {
     id: 2,
-    title: "Responsive Web Desing",
-    duration: "300h",
-    date: "October 2022",
-    link: "https://www.freecodecamp.org/certification/Juancaceres963/responsive-web-design",
-    image: certiImg2
+    title: "Javascript Engine V8 and the Browser",
+    duration: "7h",
+    date: "July 2023",
+    link: "https://platzi.com/p/dnijuancaceres/curso/1798-course/diploma/detalle/",
+    image: certiImg6
     },
     {
     id: 3,
-    title: "Basic CSS Grid",
-    duration: "14h",
+    title: "Legacy Javascript Algorithms and Data Structures",
+    duration: "300h",
     date: "November 2022",
-    link: "https://platzi.com/p/dnijuancaceres/curso/2474-css-grid/diploma/detalle/",
-    image: certiImg3
+    link: "https://www.freecodecamp.org/certification/Juancaceres963/javascript-algorithms-and-data-structures",
+    image: certiImg5
     },
     {
     id: 4,
@@ -42,26 +42,26 @@ export const certificates = [
     },
     {
     id: 5,
-    title: "Legacy Javascript Algorithms and Data Structures",
-    duration: "300h",
+    title: "Basic CSS Grid",
+    duration: "14h",
     date: "November 2022",
-    link: "https://www.freecodecamp.org/certification/Juancaceres963/javascript-algorithms-and-data-structures",
-    image: certiImg5
+    link: "https://platzi.com/p/dnijuancaceres/curso/2474-css-grid/diploma/detalle/",
+    image: certiImg3
     },
     {
     id: 6,
-    title: "Javascript Engine V8 and the Browser",
-    duration: "7h",
-    date: "July 2023",
-    link: "https://platzi.com/p/dnijuancaceres/curso/1798-course/diploma/detalle/",
-    image: certiImg6
+    title: "Responsive Web Desing",
+    duration: "300h",
+    date: "October 2022",
+    link: "https://www.freecodecamp.org/certification/Juancaceres963/responsive-web-design",
+    image: certiImg2
     },
     {
     id: 7,
-    title: "Azure Fundamentals",
-    duration: "60h",
-    date: "August 2023",
-    link: "https://www.credly.com/badges/ad1bb2df-d4d8-4399-9157-e15371cdef06/linked_in_profile",
-    image: certiImg7
-    }
+    title: "Javascript fundamentals",
+    duration: "19h",
+    date: "October 2022",
+    link: "https://platzi.com/p/dnijuancaceres/curso/1814-course/diploma/detalle/",
+    image: certiImg1
+    },
 ]
